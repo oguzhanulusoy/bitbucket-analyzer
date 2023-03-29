9b978fcc16e0ee19f43c788cc22267b9281f6f56
ayilmaz <ali.yilmaz@orioninc.com>
2023-03-29 14:16:37 +0300

    fix

--- ui/src/ui-component/user/ConfirmDialog.js
+++ ui/src/ui-component/user/ConfirmDialog.js
@@ -1,48 +1,60 @@
 import React, {useEffect} from 'react'
 import { useNavigate } from "react-router-dom";
-import ConfirmBox from "react-dialog-confirm";
-import '../../../node_modules/react-dialog-confirm/build/index.css';
-import LOGOIMG from "../../assets/images/orion_logo.png"
 import {useDispatch } from 'react-redux'
-import styled from "styled-components";
-import { blue } from '@mui/material/colors';
+
 import { SET_MENU } from '../../store/actions';
 import {useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import '../../../node_modules/react-confirm-alert/src/react-confirm-alert.css';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
 
 const ConfirmDialog = () => {
+  const [open, setOpen] = React.useState(false);
     let navigate = useNavigate();
     const theme = useTheme();
   
     const matchDownMd = useMediaQuery(theme.breakpoints.down('lg'));
-    const dispatch = useDispatch();  
+    const dispatch = useDispatch();
+    
+    
     const handleConfirm = () => {
-      return navigate("/."); }
-    const handleCancel = () => { alert('Please click YES to load data!'); }
-useEffect(() => {
-  dispatch({ type: SET_MENU, opened: true });
+      return navigate("/."); 
+    }
 
 
-}, [])
+    const handleClose = () => { 
+      alert('Please click YES to load data!'); }
+      useEffect(() => {
+      dispatch({ type: SET_MENU, opened: true });
+    }, [])
 
   return (
-    <ConfirmBox // Note : in this example all props are required
-    options={{
-      color:"$ff45dd",
-      icon:LOGOIMG,
-      text: `There is no data to show! Would you want to load data?`,
-      confirm: 'YES',
-      cancel: 'NO',
-      btn: true,
-      color:"blue",
-      backgroundColor:"red"
-    }}
-    isOpen={true}
-    onConfirm={handleConfirm}
-    onCancel={handleCancel}
-  />
+    <Dialog
+    open={open}
+    onClose={handleClose}
+    PaperComponent={PaperComponent}
+    aria-labelledby="draggable-dialog-title"
+  >
+    <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          Warning
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          There is no data to show! Would you want to load data?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button  onClick={handleClose}>
+            NO
+          </Button>
+          <Button onClick={handleConfirm}>YES</Button>
+        </DialogActions>
+     </Dialog>
   )
 }
 
